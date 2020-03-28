f980a37d63bdef6db1271409b26788cc232e3590
Cyrus <cyrus.tanade.16@ucl.ac.uk>
2020-03-28 21:34:13 +0700

    separated hyperparameters to a yaml file

--- UNet_3D_Cy/training.py
+++ UNet_3D_Cy/training.py
@@ -6,18 +6,31 @@ from keras.optimizers import Adam, SGD
 from tqdm import tqdm
 import random
 from metrics import dice_coef, dice_coef_loss
+import yaml
 
-img_width = 128
-img_height = 128
-img_thickness = 32
-img_channels = 1
+# Hyperparameters
+config = yaml.safe_load(open("hyper_parameters.yaml"))
+path_to_data = config['path_to_data']
+path_to_save = config['path_to_save']
 
-path_to_data = './data/datasets-promise12'
-path_to_save = './result' 
+N = config['N']
+N_test = config['N_test']
+
+img_width = config['img_width']
+img_height = config['img_height']
+img_thickness = config['img_thickness']
+img_channels = config['img_channels']
+
+learning_rate = float(config['learning_rate'])
+epochs = config['epochs']
+val_size = config['val_size']
+dropout = config['dropout']
+batch_size = config['batch_size']
+patience = config['patience']
+
+f = config['f']
 
 # Load Training Data
-N = 50
-idx_slice = 15
 X_train = np.zeros((N, img_thickness, img_height, img_width, img_channels), dtype=np.float32)
 Y_train = np.zeros((N, img_thickness, img_height, img_width, img_channels), dtype=np.float32)
 
@@ -36,7 +49,6 @@ print('Loaded Training Data')
 print(X_train.shape)
 
 # Load Testing Data
-N_test = 30
 X_test = np.zeros((N_test, img_thickness, img_height, img_width, img_channels), dtype=np.float32)
 
 print('','','')
@@ -73,103 +85,103 @@ inputs = tf.keras.layers.Input((img_thickness, img_width, img_height, img_channe
 s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
 
 # Encoding
-c1 = tf.keras.layers.Conv3D(16, (3, 3, 3), activation='relu',
+c1 = tf.keras.layers.Conv3D(f[1], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(s)
-c1 = tf.keras.layers.Dropout(0.2)(c1)
-c2 = tf.keras.layers.Conv3D(16, (3, 3, 3), activation='relu',
+c1 = tf.keras.layers.Dropout(dropout)(c1)
+c2 = tf.keras.layers.Conv3D(f[1], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c1)
 p1 = tf.keras.layers.MaxPooling3D((2,2,2))(c1)
 
 
-c2 = tf.keras.layers.Conv3D(32, (3, 3, 3), activation='relu',
+c2 = tf.keras.layers.Conv3D(f[2], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(p1)
-c2 = tf.keras.layers.Dropout(0.2)(c2)
-c2 = tf.keras.layers.Conv3D(32, (3, 3, 3), activation='relu',
+c2 = tf.keras.layers.Dropout(dropout)(c2)
+c2 = tf.keras.layers.Conv3D(f[2], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c2)
 p2 = tf.keras.layers.MaxPooling3D((2,2,2))(c2)
 
 
-c3 = tf.keras.layers.Conv3D(64, (3, 3, 3), activation='relu',
+c3 = tf.keras.layers.Conv3D(f[3], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(p2)
-c3 = tf.keras.layers.Dropout(0.2)(c3)
-c3 = tf.keras.layers.Conv3D(64, (3, 3, 3), activation='relu',
+c3 = tf.keras.layers.Dropout(dropout)(c3)
+c3 = tf.keras.layers.Conv3D(f[3], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c3)
 p3 = tf.keras.layers.MaxPooling3D((2,2,2))(c3)
 
 
-c4 = tf.keras.layers.Conv3D(128, (3, 3, 3), activation='relu',
+c4 = tf.keras.layers.Conv3D(f[4], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(p3)
-c4 = tf.keras.layers.Dropout(0.2)(c4)
-c4 = tf.keras.layers.Conv3D(128, (3, 3, 3), activation='relu',
+c4 = tf.keras.layers.Dropout(dropout)(c4)
+c4 = tf.keras.layers.Conv3D(f[4], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c4)
 p4 = tf.keras.layers.MaxPooling3D((2,2,2))(c4)
 
 
-c5 = tf.keras.layers.Conv3D(256, (3, 3, 3), activation='relu',
+c5 = tf.keras.layers.Conv3D(f[5], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(p4)
-c5 = tf.keras.layers.Dropout(0.2)(c5)
-c5 = tf.keras.layers.Conv3D(256, (3, 3, 3), activation='relu',
+c5 = tf.keras.layers.Dropout(dropout)(c5)
+c5 = tf.keras.layers.Conv3D(f[5], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c5)
 p5 = tf.keras.layers.MaxPooling3D((2,2,2))(c5)
 
 # Decoding Layers
-u6 = tf.keras.layers.Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same',)(c5)
+u6 = tf.keras.layers.Conv3DTranspose(f[6], (2, 2, 2), strides=(2, 2, 2), padding='same',)(c5)
 u6 = tf.keras.layers.concatenate([u6, c4])
-c6 = tf.keras.layers.Conv3D(128, (3, 3, 3), activation='relu',
+c6 = tf.keras.layers.Conv3D(f[6], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(u6)
-c6 = tf.keras.layers.Dropout(0.2)(c6)
-c6 = tf.keras.layers.Conv3D(128, (3, 3, 3), activation='relu',
+c6 = tf.keras.layers.Dropout(dropout)(c6)
+c6 = tf.keras.layers.Conv3D(f[6], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c6)
 
 
-u7 = tf.keras.layers.Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same',)(c6)
+u7 = tf.keras.layers.Conv3DTranspose(f[7], (2, 2, 2), strides=(2, 2, 2), padding='same',)(c6)
 u7 = tf.keras.layers.concatenate([u7, c3])
-c7 = tf.keras.layers.Conv3D(64, (3, 3, 3), activation='relu',
+c7 = tf.keras.layers.Conv3D(f[7], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(u7)
-c7 = tf.keras.layers.Dropout(0.2)(c7)
-c7 = tf.keras.layers.Conv3D(64, (3, 3, 3), activation='relu',
+c7 = tf.keras.layers.Dropout(dropout)(c7)
+c7 = tf.keras.layers.Conv3D(f[7], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c7)
 
 
-u8 = tf.keras.layers.Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same',)(c7)
+u8 = tf.keras.layers.Conv3DTranspose(f[8], (2, 2, 2), strides=(2, 2, 2), padding='same',)(c7)
 u8 = tf.keras.layers.concatenate([u8, c2])
-c8 = tf.keras.layers.Conv3D(32, (3, 3, 3), activation='relu',
+c8 = tf.keras.layers.Conv3D(f[8], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(u8)
-c8 = tf.keras.layers.Dropout(0.2)(c8)
-c8 = tf.keras.layers.Conv3D(32, (3, 3, 3), activation='relu',
+c8 = tf.keras.layers.Dropout(dropout)(c8)
+c8 = tf.keras.layers.Conv3D(f[8], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c8)
 
 
-u9 = tf.keras.layers.Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same',)(c8)
+u9 = tf.keras.layers.Conv3DTranspose(f[9], (2, 2, 2), strides=(2, 2, 2), padding='same',)(c8)
 u9 = tf.keras.layers.concatenate([u9, c1])
-c9 = tf.keras.layers.Conv3D(16, (3, 3, 3), activation='relu',
+c9 = tf.keras.layers.Conv3D(f[9], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(u9)
-c9 = tf.keras.layers.Dropout(0.2)(c9)
-c9 = tf.keras.layers.Conv3D(16, (3, 3, 3), activation='relu',
+c9 = tf.keras.layers.Dropout(dropout)(c9)
+c9 = tf.keras.layers.Conv3D(f[9], (3, 3, 3), activation='relu',
                             kernel_initializer='he_normal', padding='same')(c9)
 
 
 outputs = tf.keras.layers.Conv3D(1, (1, 1, 1), activation='sigmoid')(c9)
 
 model = tf.keras.Model(inputs=[inputs], outputs=[outputs])
-model.compile(optimizer=tf.optimizers.Adam(1e-3), loss=dice_coef_loss, metrics=[dice_coef])
-#model.summary()
+model.compile(optimizer=tf.optimizers.Adam(learning_rate), loss=dice_coef_loss, metrics=[dice_coef])
+model.summary()
 
 # Checkpoints and Callbacks
 checkpointer = tf.keras.callbacks.ModelCheckpoint('model_pros_segmentation.h5',
                                                   verbose=1, save_best_only=True)
 callbacks = [
-            tf.keras.callbacks.EarlyStopping(patience=20, monitor='val_loss'),
+            tf.keras.callbacks.EarlyStopping(patience=patience, monitor='loss'),
             tf.keras.callbacks.TensorBoard(log_dir='logs')]
-results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=15,
-                    epochs=10000, callbacks=callbacks) 
+results = model.fit(X_train, Y_train, validation_split=val_size, batch_size=batch_size,
+                    epochs=epochs, callbacks=callbacks) 
 
 # Save the output masks
 
 idx = random.randint(0, N)
 
-preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
-preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+preds_train = model.predict(X_train[:int(X_train.shape[0]*(1-val_size))], verbose=1)
+preds_val = model.predict(X_train[int(X_train.shape[0]*(1-val_size)):], verbose=1)
 preds_test = model.predict(X_test, verbose=1)
 
 preds_train_t = (preds_train > 0.5).astype(np.uint8)
@@ -189,7 +201,7 @@ for ix in tqdm(range(len(preds_train))):
         ax2 = fig.add_subplot(132)
         plt.imshow(np.squeeze(Y_train[ix,iy,:,:]))
         ax3 = fig.add_subplot(133)
-        plt.imshow(np.squeeze(preds_train_t[ix,idx_slice,:,:]))
+        plt.imshow(np.squeeze(preds_train_t[ix,iy,:,:]))
         ax1.title.set_text('Clinical Image')
         ax2.title.set_text('Real Mask')
         ax3.title.set_text('Predicted Mask')
